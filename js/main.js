abe9570a451e4a1b327b3504ec85e1fed8e7e2da
ignacio conde <cpnacho@hotmail.com.ar>
2023-05-21 17:51:25 -0300

    arreglo primera pre entrega

--- js/main.js
+++ js/main.js
@@ -1,28 +1,35 @@
 
-//function CalcularPeso/Potencia
-
 alert ("BIENVENIDO!!!, calcule el peso-potencia de su vehiculo (cuanto menor resultado mejor peso-potencia)");
 
-let peso = parseInt(prompt("Ingrese el peso en kg de su vehiculo"));
-let potencia = parseInt(prompt("Ingrese la potencia de su vehiculo"));
-let pesopotencia = peso / potencia;
+    let Peso = parseFloat(prompt("Ingrese el peso en kg de su vehiculo"));
+    let potencia = parseFloat(prompt("Ingrese la potencia de su vehiculo"));
+    let pesopotencia = Peso / potencia;
 
-console.log("peso: " + peso  +"kg");
-console.log("potencia " + potencia + "cv");
-console.log("pesopotencia" + pesopotencia);
+    while (Peso != Peso) {
+        alert("tu vehiculo pesa:" + Peso);
+    }
 
-alert ("Tu Peso-Potencia es de:" + pesopotencia );
+    while (potencia != potencia) {
+        alert("tu vehiculo tiene :" + potencia + "cv")
+    }
 
-// function calcular potencia/cilindrada
+    alert ("Tu Peso-Potencia es de:" + pesopotencia.toFixed(2) );
+    
+    alert ("Calcule su potencia-cilindrada (cunto mayor sea el resultado indicado, mejor rendimiento en altas RPM")   
 
-alert ("calcule su potencia-cilindrada (cunto mayor sea el resultado indicado, mejor rendimiento en altas RPM")
+    function name(params) {
+        
+    }
+    
+    function calcularCilindradaPotencia(params) {
+        alert ("Calcule su potencia-cilindrada (cunto mayor sea el resultado indicado, mejor rendimiento en altas RPM")
 
-let Potencia2 = parseInt(prompt("Ingrese la Potencia de su vehiculo"));
-let cilindrada = parseFloat(prompt("Ingrese la cilindrada de su vehiculo en Litros"));
-let potenciacilindrada = Potencia2 / cilindrada;
+        let Potencia2 = parseInt(prompt("Ingrese la Potencia de su vehiculo"));
+        let cilindrada = parseFloat(prompt("Ingrese la cilindrada de su vehiculo en Litros"));
+        let potenciacilindrada = Potencia2 * cilindrada;
 
-console.log("Potencia:" + Potencia2 + "cv");
-console.log("cilindrada" + cilindrada + "litros");
-console.log("Potencia" + potenciacilindrada);
+        alert("tu potencia-cilindrada es:" + potenciacilindrada.toFixed(2));
 
-alert ("Tu Potencia-cilindra es de :" + potenciacilindrada)
+    }
+
+    calcularCilindradaPotencia();
