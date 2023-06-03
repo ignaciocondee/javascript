f10c956d846c0b13d8f868a191b466bac6900ef4
ignacioconde <cpnacho@hotmail.com.ar>
2023-06-03 20:01:18 -0300

    entrega 2 (correciones)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 class PlazoFijo {
   calcularMontoTotal(tasaAnual, monto, dias) {
     const tasaInteres = tasaAnual / 100;
@@ -7,14 +8,35 @@ class PlazoFijo {
   }
 }
 
-const tasaAnual = parseFloat(prompt('Ingrese la tasa de interés anual (%):'));
-const monto = parseFloat(prompt('Ingrese el monto a simular:'));
-const dias = parseInt(prompt('Ingrese la cantidad de días a simular:'));
+function obtenerDatosUsuario() {
+  const tasaAnual = parseFloat(prompt('Ingrese la tasa de interés anual (%):'));
+  const monto = parseFloat(prompt('Ingrese el monto a simular:'));
+  const dias = parseInt(prompt('Ingrese la cantidad de días a simular:'));
 
-if (isNaN(tasaAnual) || isNaN(monto) || isNaN(dias)) {
-  alert('Por favor, ingrese valores numéricos válidos.');
-} else {
-  const plazoFijo = new PlazoFijo('Plazo Fijo 1');
-  const montoTotal = plazoFijo.calcularMontoTotal(tasaAnual, monto, dias);
+  if (isNaN(tasaAnual) || isNaN(monto) || isNaN(dias)) {
+    alert('Por favor, ingrese un valor correcto.');
+    return null;
+  } else {
+    return {
+      tasaAnual,
+      monto,
+      dias
+    };
+  }
+}
+
+function mostrarResultado(montoTotal) {
   alert(`Cuando finalice el plazo, usted tendrá en su cuenta: ${montoTotal}`);
 }
+
+function simularPlazoFijo(plazoFijo, datosUsuario) {
+  if (datosUsuario) {
+    const { tasaAnual, monto, dias } = datosUsuario;
+    const montoTotal = plazoFijo.calcularMontoTotal(tasaAnual, monto, dias);
+    mostrarResultado(montoTotal);
+  }
+}
+
+const plazoFijo = new PlazoFijo('Plazo Fijo 1');
+const datosUsuario = obtenerDatosUsuario();
+simularPlazoFijo(plazoFijo, datosUsuario);
