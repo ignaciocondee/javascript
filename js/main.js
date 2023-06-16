8a760cf26f5d5d548aa7f622af2f41f25784db3d
ignacioconde <cpnacho@hotmail.com.ar>
2023-06-16 13:38:46 -0300

    entrega 3

--- js/main.js
+++ js/main.js
@@ -1,42 +1,45 @@
-
 class PlazoFijo {
   calcularMontoTotal(tasaAnual, monto, dias) {
     const tasaInteres = tasaAnual / 100;
     const intereses = (monto * tasaInteres * dias) / 365;
     const total = monto + intereses;
-    return Math.round(total); // Agregar Math.round() al resultado
-  }
-}
-
-function obtenerDatosUsuario() {
-  const tasaAnual = parseFloat(prompt('Ingrese la tasa de interés anual (%):'));
-  const monto = parseFloat(prompt('Ingrese el monto a simular:'));
-  const dias = parseInt(prompt('Ingrese la cantidad de días a simular:'));
-
-  if (isNaN(tasaAnual) || isNaN(monto) || isNaN(dias)) {
-    alert('Por favor, ingrese un valor correcto.');
-    return null;
-  } else {
-    return {
-      tasaAnual,
-      monto,
-      dias
-    };
+    return Math.round(total);
   }
 }
 
 function mostrarResultado(montoTotal) {
-  alert(`Cuando finalice el plazo, usted tendrá en su cuenta: ${montoTotal}`);
+  const resultadoContainer = document.querySelector('.result-container');
+  resultadoContainer.innerHTML = '';
+  
+  const resultadoElement = document.createElement('p');
+  resultadoElement.textContent = `Cuando finalice el plazo, usted tendrá en su cuenta: ${montoTotal}`;
+  resultadoContainer.appendChild(resultadoElement);
 }
 
-function simularPlazoFijo(plazoFijo, datosUsuario) {
-  if (datosUsuario) {
-    const { tasaAnual, monto, dias } = datosUsuario;
-    const montoTotal = plazoFijo.calcularMontoTotal(tasaAnual, monto, dias);
-    mostrarResultado(montoTotal);
+function simularPlazoFijo(event) {
+  event.preventDefault();
+
+  const tasaAnual = parseFloat(document.getElementById('tasaAnual').value) || 0;
+  const monto = parseFloat(document.getElementById('monto').value) || 0;
+  const dias = parseInt(document.getElementById('dias').value) || 0;
+
+  if (!tasaAnual || !monto || !dias) {
+    alert('Por favor, ingrese un valor correcto.');
+    return;
   }
+
+  const plazoFijo = new PlazoFijo();
+  const montoTotal = plazoFijo.calcularMontoTotal(tasaAnual, monto, dias);
+  mostrarResultado(montoTotal);
+
+  const datosUsuario = {
+    tasaAnual,
+    monto,
+    dias
+  };
+
+  localStorage.setItem('datosUsuario', JSON.stringify(datosUsuario));
 }
 
-const plazoFijo = new PlazoFijo('Plazo Fijo 1');
-const datosUsuario = obtenerDatosUsuario();
-simularPlazoFijo(plazoFijo, datosUsuario);
+const plazoFijoForm = document.getElementById('plazoFijoForm');
+plazoFijoForm.addEventListener('submit', simularPlazoFijo);
